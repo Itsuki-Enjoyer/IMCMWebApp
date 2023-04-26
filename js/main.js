eb61bbf9e1951b9a026870833db3beddd9079317
Mohsen <101263055+Itsuki-Enjoyer@users.noreply.github.com>
2023-04-26 21:31:30 +0200

    Update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const form =document.querySelector('.form'),
+input = document.querySelector('.find'),
+show = document.querySelector('.show'),
+showLyric = document.querySelector('.show-lyric'),
+close = document.querySelector('.close')
+titles = document.querySelector('.titles'),
+spinner = document.querySelector('spinner'),
+navigation = document.querySelector('.navigation');
+
+
+
+// Lyrics finden
+
+const findLyrics = async(inputValue)=>{
+    
+}
